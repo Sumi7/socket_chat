4e7356d048f36730516f79ed9dd3af07907cb538
Sumit <sumitsati7@gmail.com>
2018-04-30 19:28:47 +0530

    time stamp added

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,30 @@
 var socket = io();
 
-socket.on('connect', function(){
-  console.log('connected to server');
+socket.on('connect', function() {
+    console.log('connected to server');
 });
 
-socket.on('newMessage', function({from, text}){
-  var msg = `<div>${from}: ${text}</div>`;
-  $('.msg_container').append(msg);
+socket.on('newMessage', function({ from, text, createdAt }) {
+    var time = moment(createdAt).format('h:mm a');
+    var msg = `<div>
+                    <div>${from}</div>
+                    <div>
+                        ${text}
+                        <div>${time}</div>
+                    </div>
+                </div>`;
+    $('.msg_container').append(msg);
 });
 
-socket.on('disconnect', function(){
-  console.log('disconnected from server');
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
 });
 
-$('#msg_form').on('submit', function(e){
-  e.preventDefault();
-  var msg_input = $('input[name="message"]');
-  socket.emit('createMessage', {
-    from: 'User 1',
-    text: msg_input.val()
-  }, () => msg_input.val(""));
+$('#msg_form').on('submit', function(e) {
+    e.preventDefault();
+    var msg_input = $('input[name="message"]');
+    socket.emit('createMessage', {
+        from: 'User 1',
+        text: msg_input.val()
+    }, () => msg_input.val(""));
 })
--- util/messageGenerator.js
+++ util/messageGenerator.js
@@ -1,5 +1,10 @@
+var moment = require('moment');
 function generateMessage(from, text){
-  return {from, text};
+  return {
+      from,
+      text,
+      createdAt: moment().valueOf()
+  };
 }
 
 module.exports = generateMessage;
