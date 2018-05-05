9d5899266abd5ebd9748e00a64799b19056b70ce
Sumit <sumitsati7@gmail.com>
2018-05-05 16:33:57 +0530

    users list and chat interface completed

---
+++ public/js/chat.js
@@ -0,0 +1,71 @@
+var socket = io();
+
+function scrollToBottom(){
+    var messages = $('.msg_container');
+    var newMessage = messages.children('div.message_wrapper:last-child');
+    console.log(newMessage);
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    console.log(clientHeight, scrollTop, scrollHeight, newMessageHeight, lastMessageHeight);
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+socket.on('connect', function() {
+    var params = getParams();
+    console.log(params)
+    socket.emit('join', params, function(err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('noerr')
+        }
+    })
+});
+
+socket.on('newMessage', function({ from, text, createdAt }) {
+    var time = moment(createdAt).format('h:mm a');
+    var msg = `<div class="message_wrapper">
+                    <div>
+                        <span>
+                            <h5>${from}</h5>
+                        <span>
+                        <span>
+                            <p>${time}</p>
+                        </span>
+                    </div>
+                    <div>
+                        <p>${text}</p>
+                    </div>
+                </div>`;
+    $('.msg_container').append(msg);
+    scrollToBottom();
+});
+
+socket.on('updateUserList', function(users){
+    var ol = $('<ol></ol>');
+    users.forEach(function(user){
+        ol.append($('<li></li>').text(user));
+    })
+    $('#users').html(ol);
+})
+
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
+$('#msg_form').on('submit', function(e) {
+    e.preventDefault();
+    var msg_input = $('input[name="message"]');
+    socket.emit('createMessage', {
+        from: 'User 1',
+        text: msg_input.val()
+    }, () => msg_input.val(""));
+})
--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 //file imports
-const generateMessage = require('../util/messageGenerator');
+const generateMessage = require('../utils/messageGenerator');
+const {isRealString} = require('../utils/validation');
+const {Users} = require('../utils/users');
 
 //declarations
 const app = express();
@@ -16,23 +18,38 @@ const PATH = path.join(__dirname, '../public');
 
 const SERVER = http.createServer(app);
 const io = socketIO(SERVER);
+var users = new Users();
 
 app.use(express.static(PATH));
 
 io.on("connection", (socket) => {
   console.log("new connection...")
 
-  socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat'));
+  socket.on('join', (params, cb) => {
+      if(!isRealString(params.name) || !isRealString(params.room)){
+          cb('Name and room name are required')
+      }
+      socket.join(params.room);
+      users.removeUser(socket.id);
+      users.addUser(socket.id, params.name, params.room);
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+      io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+      socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat'));
+      socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+      cb();
+  });
 
   socket.on('createMessage', (msg, cb) => {
     io.emit('newMessage', generateMessage(msg.from, msg.text));
     cb();
   });
 
-  socket.on('disconnect', (msg) =>{
-    console.log('user was disconnected');
+  socket.on('disconnect', () =>{
+    var user = users.removeUser(socket.id);
+    if(user){
+        io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+        io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+    }
   });
 
 });
